62c3fb479bc44697ef679d4cca556e24f6aff615
Mahmoud Samy <mahmoudsamy@192.168.1.13>
2024-10-15 23:01:11 +0300

    update

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -2,6 +2,6 @@
 """ Basic Annotations"""
 
 
-def to_str(n: float) -> str:
-    """floot function"""
-    return str(n)
+def to_str(n: float) -> int:
+  """takes a float n as argument"""
+  return str(n)

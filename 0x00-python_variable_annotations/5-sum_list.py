7e1a9dbbcdd6180af9ecef29686f9b20f23cf23c
Mahmoud Samy <mahmoudsamy@192.168.1.13>
2024-10-15 23:13:25 +0300

    update

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,9 +1,2 @@
 #!/usr/bin/env python3
 """ Basic Annotations"""
-
-from typing import List
-
-
-def sum_list(input_list: List[float]) -> float:
-    """sumlist function"""
-    return sum(input_list)

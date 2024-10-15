ceffcbd984b3450e9f94af18e797ac75e7b967cb
Mahmoud Samy <mahmoudsamy@192.168.1.13>
2024-10-15 22:44:37 +0300

    update

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 """ Basic Annotations"""
 
+
 import math
 
-
-def floor(n: float) -> int:
-    """floot function"""
-    return math.floor(n)
+def floor(n: float)  -> int:
+  return math.floor(n)

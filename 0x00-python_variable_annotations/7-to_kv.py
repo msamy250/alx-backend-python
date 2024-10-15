d4adbe8e3a24034c9ea540a8921d0813f0850e5a
Mahmoud Samy <mahmoudsamy@192.168.1.13>
2024-10-15 23:14:35 +0300

    update

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,4 @@
 #!/usr/bin/env python3
 """ Basic Annotations"""
 
-from typing import List, Union, Tuple
 
-
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
-    """to_kv function"""
-    return (k, v ** 2)
